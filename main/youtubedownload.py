00231397ac0f7c04dd96a8228ae3e914de4e071c
Miyandi279 <58814251+miyandi279@users.noreply.github.com>
2021-02-01 10:55:59 +0700

    Update youtubedownload.py

--- main/youtubedownload.py
+++ main/youtubedownload.py
@@ -1,6 +1,8 @@
+#bin/python
 #code by @Miyandi279
 #team : Bengkulu Cyber Team
-
+#[jangan di recode ya gan]
 import webbrowser
 url=input ("masukan url nya gan")
-url=url[:12 l
+url=url[:12]+"ss"+url[:12:]
+webbrowser.open(url)
